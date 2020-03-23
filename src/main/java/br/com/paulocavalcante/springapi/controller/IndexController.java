981041fb17af6535da18fc52c90789196f541081
Paulo Cavalcante <paulo.silva@picpay.com>
2020-03-23 14:34:51 -0300

    Add Controllers

--- src/main/java/br/com/paulocavalcante/springapi/controller/IndexController.java
+++ src/main/java/br/com/paulocavalcante/springapi/controller/IndexController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +24,11 @@ public class IndexController {
     @Autowired
     UserReposirory userReposirory;
 
+    @GetMapping(value = "/users/{id}/relatorio")
+    public ResponseEntity<User> relatorio(@PathVariable Long id) {
+        return new ResponseEntity(userReposirory.findById(id), HttpStatus.OK);
+    }
+
     @GetMapping(value = "/users/{id}")
     public ResponseEntity<User> init(@PathVariable Long id) {
         return new ResponseEntity(userReposirory.findById(id), HttpStatus.OK);
@@ -31,4 +38,12 @@ public class IndexController {
     public ResponseEntity<User> list() {
         return new ResponseEntity(userReposirory.findAll(), HttpStatus.OK);
     }
+
+    @PostMapping(value = "/users")
+    public ResponseEntity<User> cadastrar(@RequestBody User user){
+
+        return new ResponseEntity<User>(userReposirory.save(user), HttpStatus.OK);
+    }
+
+
 }
